49631d47c64bd8ee98fdf70a0a9a010b384bb272
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-07-05 15:04:10 -0300

    habilitando o cors

--- payfast/config/express.js
+++ payfast/config/express.js
@@ -9,6 +9,12 @@ var logger = require('../services/loggerService');
 module.exports = function () {
     const app = express();
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.use(morgan("common", {
         stream: {
             write: function (mensagem) {
